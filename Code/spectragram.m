5dd26aa5ab1c3b050d68cc852204a9c3d56a7278
noahterickson <enoah@pdx.edu>
2015-02-04 15:15:23 -0800

    Homework upload and code update

--- Code/spectragram.m
+++ Code/spectragram.m
@@ -1,61 +1,70 @@
 function spectragram
-close all;
-% Sample plot for GUI configuration
-a=gca; %create axes
+    close all;
+    
+    a=gca; %create axes
 
-%x=0:1e-2:6.7*pi;
-%amp = 5000;%test variable
-%y=amp*cos(x)+5000; %y
-%p=plot(x,y); %plot cos function
-%xmax=max(x); %what is this
-
-% Record your voice for 5 seconds.
-recObj = audiorecorder;
-disp('Start speaking.')
-recordblocking(recObj, 5);
-disp('End of Recording.');
-% Play back the recording.
-play(recObj);
-% Store data in double-precision array.
-myRecording = getaudiodata(recObj);
-myRecording2 = myRecording*5000+5000;
-% Plot the waveform.
-plot(myRecording2);
-
-%Set appropriate axis limits and settings
-set(gcf,'doublebuffer','on',...
+    %Set appropriate axis limits and settings
+    set(gcf,'doublebuffer','on',...
     'units','pixels',...
     'position',[40,40,840,640]);
-set(a,'xlim',[0 2]); %Start x-axis range 0-2s
-set(a,'ylim',[0 100]);%Start y-axis range 0-100Hz
+    set(a,'xlim',[0 20]); %Start x-axis range 0-2s
+    set(a,'ylim',[0 10000]);%Start y-axis range 0-100Hz
 
-% Relative positiong for sliders and buttons for resizeability
-pos=get(a,'position'); %pos(1) is left - pos(2) is bottom - pos(3) is width - pos(4) is height
-hSliderPos=[pos(1) pos(2)-0.1 pos(3) 0.05];
-vSliderPos=[pos(1)-0.1 pos(2) .05 pos(4)];
-fsBoxPos=[pos(2)+pos(3)+.05 pos(4) .1 .05];
+    % Relative positiong for sliders and buttons for resizeability
+    pos=get(a,'position'); %pos(1) is left - pos(2) is bottom - pos(3) is width - pos(4) is height
+    hSliderPos=[pos(1) pos(2)-0.1 pos(3) 0.05];
+    vSliderPos=[pos(1)-0.1 pos(2) .05 pos(4)];
+    fsBoxPos=[pos(2)+pos(3)+.05 pos(4) .1 .05];
 
+    %Callback functions for the GUI interactivity
+    hSlideCallback=['set(gca,''xlim'',[0 get(gcbo,''value'') ])'];
+    vSlideCallback=['set(gca,''ylim'',[0 get(gcbo,''value'') ])'];
+    fsBoxCallback=['set(amp,str2num(get(gcbo,''string'')))'];
 
-%Callback functions for the GUI interactivity
-hSlideCallback=['set(gca,''xlim'',[0 get(gcbo,''value'') ])'];
-vSlideCallback=['set(gca,''ylim'',[0 get(gcbo,''value'') ])'];
-fsBoxCallback=['set(amp,str2num(get(gcbo,''string'')))'];
+    %GUI Elements
+    hSlide = uicontrol('style','slider',...
+          'units','normalized','position',hSliderPos,...
+          'callback',hSlideCallback,'min',2,'max',20,'value',2,... 
+          'SliderStep',[0.0556 .2778]); %Change max and min here to change sliders
 
+    vSlide = uicontrol('style','slider',...
+          'units','normalized','position',vSliderPos,...
+          'callback',vSlideCallback,'min',100,'max',10000,'value',100,...
+          'SliderStep',[0.0556 .2778]);
 
-%GUI Elements
-hSlide = uicontrol('style','slider',...
-      'units','normalized','position',hSliderPos,...
-      'callback',hSlideCallback,'min',2,'max',50000,'value',2,...
-      'SliderStep',[0.0556 .2778]);
+    fsBox = uicontrol('style','edit',...
+          'units','normalized','Position',fsBoxPos,...
+          'String','0',...
+          'callback',fsBoxCallback);
+    loop = 0;
+    fs = 8000; %sampling frequency, to be a GUI element later
+    recordTime = .5; % 5 milliseconds of recording, refresh rate of sorts
+    period = 0:1/(fs):recordTime; %period is 1/sampling frequency until recordtime
+    timeLength = (fs*recordTime*20); %How long does the time array need to be
+    timeArray = 0:1/fs:20; %Create time array, store data from 20 seconds
+    buffer = timeArray; %Initiliaze the buffer array
+    x=1;
+    while x==1 %infinite loop, CTRL+C to stop
+                
+        myRecording = period; %initialize the myRecording to be same as period
+        recObj = audiorecorder(fs,8,1); % initiliazes microphone with sampling frequency 
 
-vSlide = uicontrol('style','slider',...
-      'units','normalized','position',vSliderPos,...
-      'callback',vSlideCallback,'min',100,'max',10000,'value',100,...
-      'SliderStep',[0.0556 .2778]);
-  
-fsBox = uicontrol('style','edit',...
-      'units','normalized','Position',fsBoxPos,...
-      'String','0',...
-      'callback',fsBoxCallback);
+        recordblocking(recObj, recordTime); %Records until recordtime
+        myRecording = getaudiodata(recObj); %get audio in
+        myRecording2 = myRecording*5000+5000; %scale array for hw purposes
 
-
+        myRecording3 = rot90(myRecording2); %Rotates the recorded array
+        myRecording3 = [myRecording3, zeros(1, length(period) - length(myRecording3))]; %pads zeros to meet period array
+        
+        i = 1;
+        while i<recordTime*fs+1 %loop until i is equal to the length of myRecording [use length(myRecording3)??]
+            buffer(i+loop*recordTime*fs) = myRecording3(i);
+            i=i+1;
+        end
+        
+        loop=loop+1;
+        plot(timeArray,buffer);
+        drawnow
+        buffer = circshift(buffer,-(recordTime*fs+1));
+        timeArray = circshift(timeArray, -(recordTime*fs+1));
+  end
